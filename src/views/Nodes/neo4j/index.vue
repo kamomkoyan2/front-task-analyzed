e49e2e5f6a9c68f543ecff16e1d38572a20efe3c
Kamo Mkoyan <kamo.mkoyan@matemat.io>
2023-07-10 00:27:57 +0400

    ** fixes **

--- src/views/Nodes/neo4j/index.vue
+++ src/views/Nodes/neo4j/index.vue
@@ -156,7 +156,7 @@ onMounted(() => {
         v-model="form.targetNodeId"
         required
     />
-      <Button as="button" type="submit" class=" cursor-pointer inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+      <Button as="button" :disabled="(form.sourceNodeId === '' || form.sourceNodeId === null) || (form.targetNodeId === '' || form.targetNodeId === null)" type="submit" class=" cursor-pointer inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
         Connect Nodes
       </Button>
     </form>
